ff8ff6e40e3664595bcd38d17dd00ff2a8615472
ga7axy <akshaymrajput@gmail.com>
2023-01-30 15:31:38 +0530

    crawling page recursively

--- crawl.js
+++ crawl.js
@@ -1,27 +1,52 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
-  console.log(`crawling ${currentURL}`);
+async function crawlPage(baseURL, currentURL, pages) {
+  const currentURLObj = new URL(currentURL);
+  const baseURLObj = new URL(baseURL);
 
+  if (currentURLObj.hostname !== baseURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
+  console.log(`crawling ${currentURL}`);
+  let htmlBody = "";
   try {
     const response = await fetch(currentURL);
     if (response.status > 399) {
       console.log(
         `error in fetch with status code ${response.status} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
-    const contentType = reponse.headers.get("content-type");
+    const contentType = response.headers.get("content-type");
     if (!contentType.includes("text/html")) {
       console.log(
         `non html response, content type ${contentType} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
-    console.log(await response.text());
+    htmlBody = await response.text();
   } catch (error) {
-    console.log(`error in fetch because ---> ${error.message}`);
+    console.log(
+      `error in fetch because ---> ${error.message}, on page ${currentURL}`
+    );
   }
+
+  const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+  for (const nextURL of nextURLs) {
+    pages = await crawlPage(baseURL, nextURL, pages);
+  }
+
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
@@ -50,11 +75,11 @@ function getURLsFromHTML(htmlBody, baseURL) {
 
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
-  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
-  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
-    return hostPath.slice(0, -1);
+  let fullPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
   }
-  return hostPath;
+  return fullPath;
 }
 
 module.exports = {
